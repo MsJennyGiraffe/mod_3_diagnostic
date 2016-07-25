c35ee492c7eabc87f96c33c78bd5c2020b9bf1f4
Jennifer <jennifersoden@hotmail.com>
2016-07-25 09:54:28 -0600

    Add feature test for user can search

---
+++ spec/features/user_can_enter_a_zipcode_and_get_results_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search?zip=80203"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
+RSpec.feature "User can search" do
+  describe "user can enter a zipcode and see results" do
+    visit "/"
+    fill_in "80203"
+    click_button "Locate"
+
+    expect(current_path).to eq("/search?zip=80203")
+    expect(page).to have_content("stations")
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Addess")
+    expect(page).to have_content("Fuel Types")
+    expect(page).to have_content("Distance")
+    expect(page).to have_content("Access Times")
+  end
+end
