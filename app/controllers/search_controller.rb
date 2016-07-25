463565a754462dfccc1115e8c10f4ae0200da95c
Jennifer <jennifersoden@hotmail.com>
2016-07-25 10:47:12 -0600

    Service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
     @zipcode = params[:q]
+    @station = NrelService.new.get_station
   end
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,6 +5,16 @@
 # &api_key=nrel_key
 # &format=JSON
 
+# https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=sZbgdQHYQQLBha3dhHsaQddI3NzKTe3fvKOJY3Sg&format=JSON
+
 # GET /api/alt-fuel-stations/v1/nearest.format?parameters
 
-response = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1.json')
+class NrelService
+  def initialize
+    @connection = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1.json')
+  end
+
+  def get_station#(zipcode)
+    @connection.get("fuel_type=E85,ELEC&state=CA&limit=2&api_key=nrel_key&format=JSON")
+  end
+end
